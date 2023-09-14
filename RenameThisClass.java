a4d8c4a8abc66dd4fc2496aa6c511838f2132e8c
Eva Bartolome <eva.bartolome@mrjaffesclass.com>
2023-09-14 11:20:50 -0700

    Commit message

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,9 +9,10 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         int i = 10;
-        int f = i++; //adding postfix
+        int f = i + 0; //initialzed f as 0 
         boolean flag = true;
         flag =!flag;
+        i++; //adding postfix
         System.out.println("i="+i); //easier to match variables being printed
         System.out.println("flag="+flag);
         System.out.println("f="+f);
